e1bad4e10b4699d8549047e54b83df4da329aad7
wangl <394590459@qq.com>
2018-01-13 17:23:16 +0800

    初始化项目

---
+++ app/scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name codelabApp
+ * @description
+ * # codelabApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('codelabApp', ['ui.router', 'wl.ng.component'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .state('mytodo', {
+        url: '/mytodo',
+        templateUrl: 'views/mytodo.html',
+        controller: 'MytodoCtrl',
+        controllerAs: 'mytodo'
+      })
+      .state('component', {
+        url: '/component',
+        templateUrl: 'views/component.html',
+        controller: 'ComponentCtrl',
+        controllerAs: 'component'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      });
+    $urlRouterProvider.otherwise('/home');
+  });
